fb01aced245ea5f7da9e45de2c6f4bac8f757b21
Anees <58396888+Zer0-XD@users.noreply.github.com>
2022-05-30 22:08:30 +0500

    Add files via upload

--- src/js/loading.js
+++ src/js/loading.js
@@ -17,26 +17,43 @@ Pace.on('done', () => {
         }, "-=2.5")
         .to('.loading__text', {
             delay: .15,
-            duration: 3,
+            duration: 1.4,
             opacity: 0,
             yPercent: -400,
             ease: "BezierEasing(0.19,1,0.22,1)"
         }, 'p')
         .to('.page', {
-            duration: 2,
-            delay: .3,
+            // zIndex: 99999,
+            duration: 1.5,
+            delay: .1,
             // y: 0,
             opacity: 1,
             ease: Expo.easeInOut
         }, '-=2.5')
-        .to('.big-circle', {
+        .to('.big-circle , .hero-title , .hero-subtitle , .stroked-text', {
             duration: 2,
             delay: .3,
-            y: -30,
+            y: -5,
             // x: 10,
-            // opacity: 1,
+            opacity: 1,
             ease: Expo.easeInOut
         }, '-=2.5')
-
-
+        .to('.bg-line', {
+            duration: 2,
+            delay: .25,
+            opacity: 1,
+        })
+        .to('#preloader', {
+            duration: 0.1,
+            delay: 0,
+            height: 0,
+            width: 0,
+            display: 'none',
+            // position: 'static'
+        })
+        .to('::-webkit-scrollbar', {
+            duration: 0.1,
+            delay: 0,
+            width: 10,
+        })
 })
